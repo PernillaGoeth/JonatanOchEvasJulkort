75cafd1ff2d6229775198f90a0c6082786a511a0
Pernilla Göth <pernilla.goth@stratsys.se>
2022-12-17 23:12:36 +0100

    First commit

---
+++ christmasCard/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="answeredCorrect">VIDEO</div>
+  <div v-if="!answeredCorrect" class="container welcome">
+    <div style="padding: 70px;">
+      <h1 style="font-size: 50px;">Välkommen till Eva och Jonatans julkort!</h1>
+    </div>
+  </div>
+  <div v-if="!answeredCorrect" class="container login">
+    <Login @correctAnswer="answeredCorrect = true"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Login from "./components/Login.vue";
+const answeredCorrect = ref<boolean>(false)
+</script>
+
+<style scoped>
+.container {
+  font-family: Arial, Helvetica, sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 50%;
+}
+
+.welcome {
+  justify-content: flex-end;
+}
+
+.login {
+  justify-content: flex-start;
+}
+</style>
---
+++ christmasCard/src/components/Login.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="loginCard">
+    <div>
+      Hur många julenötter (oavsett knäckstatus) fanns det på Pinetorpet på juldagsmorgonen?
+    </div>
+    <input type="text" ref="inputText"/>
+
+    <button v-if="tries > 1" type="button" @click="buttonIsPressed = !buttonIsPressed">
+      {{!buttonIsPressed ? ' Det här var knepigt, ge mig en ledtråd!' :  'Tack!'}}
+    </button>
+
+    <div v-if="buttonIsPressed">
+<div>
+      Först och främst, för er messerschmittrar: 
+</div>
+<div></div>
+      Nej, mandeln är visserligen ingen nöt, men den är en JULENÖT, baske oss!'Vilket är vad som efterfrågas.
+<div></div>
+      Med den kunskapen, räkna bakifrån.
+<div></div>
+      40 kvar i påsen på slutet, minus 18 från ekorren blir 22.
+<div></div>
+      Lägg sedan till alla andra nötter.
+<div></div>
+      Tänk på att katterna får tre HALVOR VAR. Lurigt där.
+<div></div>
+      Lycka till!
+    </div>
+
+
+
+    <button type="button" @click="submit">
+      Skicka in
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const emit = defineEmits<{
+  (e: 'correctAnswer'): void
+}>()
+
+const inputText = ref<HTMLInputElement>()
+const buttonIsPressed = ref<boolean>(false)
+const tries = ref<number>(0)
+
+const submit = () => {
+  if(inputText.value && inputText.value.value === '264') {
+    console.log(inputText.value.value)
+    emit('correctAnswer')
+  } else {
+    tries.value = tries.value + 1
+  }
+}
+</script>
+
+<style scoped>
+.loginCard {
+  font-family: Arial, Helvetica, sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: center; 
+  align-items: center;
+  gap: 24px;
+  min-height: 100;
+  width: 400px;
+  background-color: aquamarine;
+  border-radius: 10px;
+  padding: 24px;
+}
+</style>
