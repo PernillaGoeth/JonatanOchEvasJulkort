da4726822e3ef5ea59aec0c4d4f6e2d9b2880d81
Pernilla Göth <pernilla.goth@stratsys.se>
2022-12-25 15:16:26 +0100

    Trying without video

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div v-if="answeredCorrect" class="videoContainer">
-    <video width="800" height="500" controls autoplay>
+    <!-- <video width="800" height="500" controls autoplay>
       <source src="./movie.mp4" type="video/mp4">
-    </video>
+    </video> -->
+    Video
   </div>
   <div v-if="!answeredCorrect" class="container welcome">
     <div style="padding: 70px">
